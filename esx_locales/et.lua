d8b5d028fe12934bc18bacd36fb8222f06926b35
Walls <blumvaido@gmail.com>
2023-02-12 23:28:31 +0200

    Create et.lua
    Create estonian locales for the resource

---
+++ esx_locales/et.lua
@@ -0,0 +1,13 @@
+Locales['et'] = {
+    ['notify_failedminigame']= 'Sul läks nässu!',
+    ['notify_opening']= 'Avanes...',
+    ['notify_gotnothing'] = 'Sa ei saanud midagi',
+    ['notify_alreadyopen']= 'Kass on tühi...',
+    ['notify_nopick']= 'Sul pole vajaminevaid asju',
+    ['progress_opening']= 'Varastad...',
+    ['target_unlockregister']= 'Röövi poekassat',
+    ['target_cracksafe']= 'Puuri seifi',
+    ['police_bliptitle']= 'Poerööv',
+    ['police_message']= 'Poerööv',
+    ['police_code']= '10-00',
+}
